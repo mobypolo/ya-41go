2298ff05c29ccb1b47cfc0ec6895f49e4adb02b9
Moby Polo <igubanov@croc.ru>
2025-04-20 23:55:57 +0700

    [iter7] minor fix

--- internal/server/customerrors/erroshandler.go
+++ internal/server/customerrors/erroshandler.go
@@ -9,7 +9,7 @@ func ErrorHandler(err error, w http.ResponseWriter) {
 	switch {
 	case errors.Is(err, ErrUnsupportedType):
 		http.Error(w, err.Error(), http.StatusNotImplemented)
-	case errors.Is(err, ErrUnknownGaugeName) || errors.Is(err, ErrUnknownCounterName):
+	case errors.Is(err, ErrUnknownGaugeName) || errors.Is(err, ErrUnknownCounterName) || errors.Is(err, ErrNotFound):
 		http.Error(w, err.Error(), http.StatusNotFound)
 	case errors.Is(err, ErrInvalidValue):
 		http.Error(w, err.Error(), http.StatusBadRequest)
